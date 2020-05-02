65672cd06462690a6e31be8bd312aef6c6c32568
RajasG <rajasmgandhi@gmail.com>
2020-05-01 22:24:19 -0500

    Update gradescraper.py

--- gradescraper.py
+++ gradescraper.py
@@ -59,4 +59,4 @@ def grades():
     return render_template('grades.html', title='Grades', classNames=classNames, classAverages=classAverages, length=len(classNames))
    
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, port=5000)
